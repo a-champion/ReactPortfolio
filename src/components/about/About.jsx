91085d8ab4aff5ae00b436f29902c9456f2df4c3
T <98498029+a-champion@users.noreply.github.com>
2024-09-30 01:09:31 -0400

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,19 +8,19 @@ const About = () => {
     <section id='about'>
       <h5>Get To Know</h5>
       <h2>About Me</h2>
-      <div className='container about_container'>
+      {/* <div className='container about_container'>
         <div className='about_me'>
           <div className="about_me-image">
             <img src={US} alt='About Image' />
           </div>
-        </div>
+        </div> */}
         
         <div className='about_content'>
           <div className="about_cards">
             <article className='about_card'>
               <FaAward className='about_icon'/>
               <h5>Who Am I</h5>
-              <small>A software engineer, husband, and father with a passion for learning.</small>
+              <small>A software engineer, content creator, artist, and father with a passion for trying news things.</small>
             </article>
 
             <article className='about_card'>
@@ -39,7 +39,7 @@ const About = () => {
             
           </div>
           <p>
-            I am a new software engineer transitioning from sales and retail with many years of customer service experience. My time in the military has prepared me for cross-team collaboration in fast paced envrionments, and my love of gaming naturally turned into a love of healthy competition! I use the knowledge and experience that I have obtained to work efficiently in high-stress situations, analyze my team's strengths, and delegate tasks, in order to meet client expectations... just don't ask me where to stand. I'm not the raid leader.
+            I am a new software engineer transitioning from sales and retail with many years of customer service experience. My time in the military has prepared me for cross-team collaboration in fast paced envrionments. I use the knowledge and experience that I have obtained to work efficiently in high-stress situations, analyze my team's strengths, and delegate tasks, in order to meet client expectations. Just don't ask me where to stand, I'm not a raid leader.
             </p>
             <a href="#contact" className='btn btn-primary'>Let's Talk</a>
         </div>
